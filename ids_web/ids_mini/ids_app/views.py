3bfccea19ac41161536e74c1de67696021f50aa5
rhish <rhish9h@gmail.com>
2019-10-10 22:43:38 +0530

    dict conv & front end

--- ids_web/ids_mini/ids_app/views.py
+++ ids_web/ids_mini/ids_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+import pickle
+import os
 # from django.http import HttpResponse
 # from django.template import loader
 # Create your views here.
 
 def index(request):
+
+  # load pickle
+  modulePath = os.path.dirname(__file__) #path name of current directory
+  filePath = os.path.join(modulePath, 'drop_down_pickle')
+  drop_file = open(filePath, 'rb')
+  ddown = pickle.load(drop_file)
+  drop_file.close()
+
+
+  if request.method == 'POST':
+    model = request.POST.get('modelChosen', '')
+    prediction = 'predicted value'
+    return render(request, 'ids_app/index.html', {'model': model, 'ddown': ddown})
   # template = loader.get_template('ids_app/index.html')
   # return HttpResponse(template.render())
-  return render(request, 'ids_app/index.html')
+
+  return render(request, 'ids_app/index.html', {'ddown': ddown})
